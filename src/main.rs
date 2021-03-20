a24829bc9475da3c5ebe40518ff3868da35cc6d1
Andrew Graham-Yooll <andrewgy8@gmail.com>
2021-03-20 22:46:01 +0100

    Generate matrix from coordinates

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,38 @@ use std::collections::{BTreeMap};
 use std::time::SystemTime;
 use std::process;
 use osm4routing::models::{Node, Edge};
-use haversiner::{haversine, Point, Measure};
 use std::fs::OpenOptions;
 use std::cmp::min;
 
 fn main() {
-    let file_name = "mad1.osm.pbf";
     let now = SystemTime::now();
+    let file_name: String = String::from("mad1.osm.pbf");
 
-    let (nodes, edges) = parse_graph(file_name);
-
+    let map = Map::from(&file_name);
     let mut graph: Graph<(), f64, Directed> = Graph::new();
     let mut nodes_on_graph = BTreeMap::new();
     let mut index_on_graph = BTreeMap::new();
 
-    let start_coordinates = (40.424725, -3.690438);
-    let end_coordinates = (40.421096, -3.688578);
-    let inferred_start_node = get_node_for_coordinates(start_coordinates.0, start_coordinates.1, nodes.clone());
-    let inferred_end_node = get_node_for_coordinates(end_coordinates.0, end_coordinates.1, nodes.clone());
-
-    let START_NODE_ID: i64 = 25903298;
-    let END_NODE_ID: i64 = 942080448;
-    assert_eq!(inferred_start_node.id, START_NODE_ID);
-    assert_eq!(inferred_end_node.id, END_NODE_ID);
-    let mut end_node = None;
-
-    for node in nodes {
-        let new_node = graph.add_node(());
-        nodes_on_graph.insert(node.id, new_node);
-        index_on_graph.insert(new_node, node);
-        if node.id == END_NODE_ID {
-            end_node = Some(new_node);
-        }
-    }
-    let start_node = nodes_on_graph.get(&START_NODE_ID).unwrap().clone();
-    if end_node == None {
-        println!("The node {} is unknown. Are you sure it is the intersection of two roads?",
-                 END_NODE_ID);
-        process::exit(1);
+    let mut coordinate_input: Vec<(f64, f64)> = vec![
+        (40.424725, -3.690438),
+        (40.421096, -3.688578),
+        (40.421834, -3.693954)
+    ];
+    let mut coordinates: Vec<Coordinate> = Vec::new();
+    for coordinate in coordinate_input {
+        let mut coord = Coordinate::from(coordinate);
+        let node = map.reverse_geocode_node(coordinate);
+        coord.node = Some(node);
+        coordinates.push(coord);
     }
 
-    for edge in edges.iter().filter(|&e| {
+    for node in map.nodes {
+        let graph_node = graph.add_node(());
+        nodes_on_graph.insert(node.id, graph_node);
+        index_on_graph.insert(graph_node, node);
+    }
+
+    for edge in map.edges.iter().filter(|&e| {
         e.properties.car_forward >= 1
     }) {
         let start = nodes_on_graph.get(&edge.source).unwrap().clone();
@@ -53,59 +44,87 @@ fn main() {
         let weight = edge.length();
         graph.add_edge(start, end, weight);
     }
-
     println!("Nodes on graph, {}", graph.node_count());
     println!("Edges on graph, {}", graph.edge_count());
-    println!(" ✓ duration: {}s\n", now.elapsed().unwrap().as_secs());
+    let mut result: Vec<Vec<f64>> = Vec::new();
 
-    let res = astar(&graph, start_node, |finish| finish == end_node.unwrap(), |e| *e.weight(),  |_| 0.);
-    let (cost, path) = res.unwrap();
-    println!(" ✓ duration: {}s\n", now.elapsed().unwrap().as_secs());
-    println!("Path count: {}\n", path.len());
-    println!("Result cost: {}\n", cost);
+    for origin_coordinate in &coordinates {
+        let start_node = nodes_on_graph.get(&origin_coordinate.node.unwrap().id).unwrap().clone();
+        let mut distances: Vec<f64> = Vec::new();
 
-
-    for value in path {
-        let node = index_on_graph.get(&value).unwrap().clone();
-
-        println!("{:?}, {:?}", node.coord.lat, node.coord.lon);
-    }
-
-
-// z is not inside res because there is not path from b to z.
-}
-
-
-fn parse_graph(file_name: &str) -> (Vec<Node>, Vec<Edge>) {
-    let (nodes, edges) = osm4routing::reader::read(file_name).expect("Read OSM file");
-    return (nodes, edges);
-}
-
-fn get_node_for_coordinates(lat: f64, lon: f64, nodes: Vec<Node>) -> Node {
-    let mut min_distance: Option<f64> = None;
-    let mut node: Option<Node> = None;
-    for current_node in nodes {
-        let calc_distance: Option<f64> = Some(haversine_distance(lat, lon, current_node.coord.lat, current_node.coord.lon));
-        if min_distance.is_none() {
-            min_distance = calc_distance;
-            node = Some(current_node);
-        } else if calc_distance.unwrap() < min_distance.unwrap() {
-            min_distance = calc_distance;
-            node = Some(current_node);
+        for destination_coordinate in &coordinates {
+            let end_node = nodes_on_graph.get(&destination_coordinate.node.unwrap().id).unwrap().clone();
+            let res = astar(&graph, start_node, |finish| finish == end_node, |e| *e.weight(), |_| 0.);
+            let (cost, path) = res.unwrap();
+            distances.push(cost);
         }
+
+        result.push(distances);
     }
-    return node.unwrap();
+
+    println!(" ✓ duration: {}s\n", now.elapsed().unwrap().as_secs());
+    println!("Costs: {:?}s\n", result);
+
 }
 
-fn haversine_distance(origin_lat: f64, origin_lon: f64, dest_lat: f64, dest_lon: f64) -> f64 {
-    static R: f64 = 6372.8;
-    let diff = origin_lon - dest_lon;
-    let ori_lon = diff.to_radians();
-    let ori_lat = origin_lat.to_radians();
-    let dest_lat = dest_lat.to_radians();
-    let dz: f64 = ori_lat.sin() - dest_lat.sin();
-    let dx: f64 = ori_lon.cos() * ori_lat.cos() - dest_lat.cos();
-    let dy: f64 = ori_lon.sin() * ori_lat.cos();
-    let dist: f64 = ((dx * dx + dy * dy + dz * dz).sqrt() / 2.0).asin() * 2.0 * R;
-    return dist
+struct Map {
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
 }
+
+impl From<&String> for Map {
+    fn from(file_name: &String) -> Map {
+        let (nodes, edges) = osm4routing::reader::read(file_name).expect("Read OSM file");
+        Self { nodes, edges }
+    }
+}
+
+impl Map {
+    fn reverse_geocode_node(&self, coordinate: (f64, f64)) -> Node {
+        let lat: f64 = coordinate.0;
+        let lon: f64 = coordinate.1;
+        let mut min_distance: Option<f64> = None;
+        let mut node: Option<Node> = None;
+        for current_node in self.nodes.clone() {
+            let calc_distance: Option<f64> = Some(
+                self.haversine_distance(lat, lon, current_node.coord.lat, current_node.coord.lon)
+            );
+            if min_distance.is_none() {
+                min_distance = calc_distance;
+                node = Some(current_node);
+            } else if calc_distance.unwrap() < min_distance.unwrap() {
+                min_distance = calc_distance;
+                node = Some(current_node);
+            }
+        }
+        return node.unwrap();
+    }
+
+    fn haversine_distance(&self, origin_lat: f64, origin_lon: f64, dest_lat: f64, dest_lon: f64) -> f64 {
+        static R: f64 = 6372.8;
+        let diff = origin_lon - dest_lon;
+        let ori_lon = diff.to_radians();
+        let ori_lat = origin_lat.to_radians();
+        let dest_lat = dest_lat.to_radians();
+        let dz: f64 = ori_lat.sin() - dest_lat.sin();
+        let dx: f64 = ori_lon.cos() * ori_lat.cos() - dest_lat.cos();
+        let dy: f64 = ori_lon.sin() * ori_lat.cos();
+        let dist: f64 = ((dx * dx + dy * dy + dz * dz).sqrt() / 2.0).asin() * 2.0 * R;
+        return dist;
+    }
+}
+
+struct Coordinate {
+    lat: f64,
+    lon: f64,
+    node: Option<Node>,
+}
+
+impl From<(f64, f64)> for Coordinate {
+    fn from(coordinate: (f64, f64)) -> Coordinate {
+        Self { lat: coordinate.0, lon: coordinate.1, node: None }
+    }
+}
+
+
+
