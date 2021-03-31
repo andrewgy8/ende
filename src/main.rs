e58cc7f2debfd7f23c4a4ef1bc706aa655393d54
andrewgy8 <andrewgy8@gmail.com>
2021-03-31 15:00:11 +0200

    Update error from locations json file

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,8 @@ fn main() {
 
     let args = Docopt::new(USAGE).unwrap().parse().unwrap_or_else(|e| e.exit());
 
-    let locations_file = fs::File::open(args.get_str("<locations.json>")).expect("file should open read only");
-    let coordinate_input: Vec<(f64, f64)> = serde_json::from_reader(locations_file).expect("file should be proper JSON");
+    let locations_file = fs::File::open(args.get_str("<locations.json>")).expect("Locations json not found.");
+    let coordinate_input: Vec<(f64, f64)> = serde_json::from_reader(locations_file).expect("Locations json is invalid.");
 
     println!("Generating a matrix for {} locations", coordinate_input.len());
 
